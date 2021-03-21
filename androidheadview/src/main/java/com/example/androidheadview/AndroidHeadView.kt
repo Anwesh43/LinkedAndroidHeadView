909b5ff0ef003321306c336dba05a52e2e1bd2d4
Anwesh43 <anweshthecool0@gmail.com>
2021-03-22 01:56:32 +0530

    adding constants and libraries

--- androidheadview/src/main/java/com/example/androidheadview/AndroidHeadView.kt
+++ androidheadview/src/main/java/com/example/androidheadview/AndroidHeadView.kt
@@ -1,2 +1,27 @@
 package com.example.androidheadview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#9C27B0",
+    "#01579B",
+    "#F57F17",
+    "#4CAF50"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val strokeFactor : Float = 90f
+val arcFactor : Float = 7.9f
+val eyeFactor : Float = 25.6f
+val lineFactor : Float = 4.5f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
