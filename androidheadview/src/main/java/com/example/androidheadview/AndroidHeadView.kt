bfacfa0644cf003e07567ca0f3f9a71ebe22511f
Anwesh43 <anweshthecool0@gmail.com>
2021-03-22 02:16:47 +0530

    adding AHNode class

--- androidheadview/src/main/java/com/example/androidheadview/AndroidHeadView.kt
+++ androidheadview/src/main/java/com/example/androidheadview/AndroidHeadView.kt
@@ -129,4 +129,45 @@ class AndroidHeadView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AHNode(var i : Int, val state : State = State()) {
+
+        private var next : AHNode? = null
+        private var prev : AHNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AHNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAHNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AHNode {
+            var curr : AHNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
