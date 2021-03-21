28d9e627b934cf76a3a60c3aac59408ab43619c9
Anwesh43 <anweshthecool0@gmail.com>
2021-03-22 02:12:52 +0530

    adding State class

--- androidheadview/src/main/java/com/example/androidheadview/AndroidHeadView.kt
+++ androidheadview/src/main/java/com/example/androidheadview/AndroidHeadView.kt
@@ -25,6 +25,7 @@ val eyeFactor : Float = 25.6f
 val lineFactor : Float = 4.5f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val scGap : Float = 0.02f / parts
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -79,5 +80,25 @@ class AndroidHeadView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scGap * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
