3ce8099576e692546119cd47e6797d24f9d64276
Anwesh43 <anweshthecool0@gmail.com>
2021-03-22 02:08:56 +0530

    adding AndroidHeadView class

--- androidheadview/src/main/java/com/example/androidheadview/AndroidHeadView.kt
+++ androidheadview/src/main/java/com/example/androidheadview/AndroidHeadView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawAHNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawAndroidHead(scale, i, w, h, paint)
 }
+
+class AndroidHeadView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
